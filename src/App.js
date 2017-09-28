8254c977387093e52951f52d83b1f2e56ee7dfd5
aarontlee10 <aarontlee10@gmail.com>
2017-09-27 19:39:36 -0700

    started new sandbox ay

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function HelloWorld(props) {
+  return (<h1>Hello, {props.name}</h1>);
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        <p className="App-intro"> </p>
+        <HelloWorld name="Aaron"/>
+      </div>
+    );
+  }
+}
+
+
+export default App;
