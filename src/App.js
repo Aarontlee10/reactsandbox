1d208dc8c6c6f4655c5eb2530a3ccef31284af33
aarontlee10 <aarontlee10@gmail.com>
2017-09-30 18:38:25 -0700

    updated timer

--- src/App.js
+++ src/App.js
@@ -2,22 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function HelloWorld(props) {
-  return (<h1>Hello, {props.name}</h1>); //Components must start with a capital letter
-}
 
-class App extends Component {
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: props.name,
+      date: new Date(),
+    };
+  }
+
+  //When the component renders, do this.
+  componentDidMount() {
+    this.timerID = setInterval(()=> this.tick(), 1000);
+  }
+
+  //When the DOM produced by Clock is removed, clear that timer
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  //Sets the new state, gives a new date instead of changing the old one for
+  //pure Component, but keeps the rest of the state the same
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
   render() {
     return (
-      <div className="App">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        <p className="App-intro"> </p>
-        <HelloWorld name="Aaron"/>
+      <div>
+        <h1>Hello, {this.state.name}</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          <Clock name="Aaron"/>
+        </p>
+      </div>
+    );
+  }
+}
 
+//
 export default App;
